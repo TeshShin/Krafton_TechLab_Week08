c7c417df6f1b9fc4734fb287d192e18d04739664
lorevoon <zoejsy22@gmail.com>
2025-10-18 15:09:34 +0900

    feat: add sphere and AABB intersection

--- Engine/Source/Physics/Private/BoundingSphere.cpp
+++ Engine/Source/Physics/Private/BoundingSphere.cpp
@@ -5,3 +5,9 @@ bool FBoundingSphere::RaycastHit() const
 {
 	return false;
 }
+
+bool FBoundingSphere::IsIntersected(const FAABB& Other)
+{
+	float sqDist = Other.GetCenterDistanceSquared(Center);
+	return sqDist <= (Radius * Radius);
+}
