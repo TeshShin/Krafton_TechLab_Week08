af4043f53c54dcc117ddf9cfb0657a373790978e
dack-c <qpxmaos3845@gmail.com>
2025-10-23 03:02:51 +0900

    fix: MaterialCache delete 로직 수정

--- Engine/Source/Manager/Private/AssetManager.cpp
+++ Engine/Source/Manager/Private/AssetManager.cpp
@@ -79,6 +79,9 @@ void UAssetManager::Initialize()
 
 void UAssetManager::Release()
 {
+	// StaticMeshCache가 Material을 삭제하므로 여기서는 배열만 clear
+	MaterialCache.clear();
+
 	// TMap.Value()
 	for (auto& Pair : VertexBuffers)
 	{
@@ -101,9 +104,6 @@ void UAssetManager::Release()
 	StaticMeshCache.clear();	// unique ptr 이라서 자동으로 해제됨
 	StaticMeshVertexBuffers.clear();
 	StaticMeshIndexBuffers.clear();
-
-	// Material Cache Release - UObject 시스템이 자동으로 관리하므로 배열만 clear
-	MaterialCache.clear();
 
 	// TMap.Empty()
 	VertexBuffers.clear();
